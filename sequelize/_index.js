0b38890be14d6478f79c4fd0e1fc28782918e53a
ricardoafonfaria <ricardoafonfaria@gmail.com>
2021-01-20 01:26:26 +0000

    Database config update

--- sequelize/_index.js
+++ sequelize/_index.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  host: '94.46.15.180',
-  port: '3306',
-  database: 'experiment',
-  username: 'experiment',
-  password: 'raafa179',
-  dialect: 'mariadb',
+  host: 'ec2-46-137-100-204.eu-west-1.compute.amazonaws.com',
+  port: '5432',
+  database: 'd2lkif2rh31r0q',
+  username: 'ympcpnywcjqdlb',
+  password: '3ac2d9d159638c0f56662b5eeacd9686c948928b07691b57b524c0f7a6e2b5fe',
+  dialect: 'postgres',
   logging: false
 });
 

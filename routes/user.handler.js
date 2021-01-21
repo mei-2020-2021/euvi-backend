c257147e5659d2e28425aad1ad8c19e342d9af9e
ricardoafonfaria <ricardoafonfaria@gmail.com>
2021-01-21 17:05:57 +0000

    fix

--- routes/user.handler.js
+++ routes/user.handler.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const User = require('../sequelize/models/user.model');
+
 const router = express.Router();
 const sequelize = require('../sequelize/_index');
-const { Op } = require('sequelize');
 const Service = require('../sequelize/models/service.model');
 
-
-
-router.post('/', async function (req, res) {
- const newUser = await User.create({
+router.post('/', async (req, res) => {
+  const newUser = await User.create({
     Uid: req.body.Uid,
     FirstName: req.body.FirstName,
     LastName: req.body.LastName,
@@ -18,16 +16,14 @@ router.post('/', async function (req, res) {
   return res.status(200).json(newUser);
 });
 
-
-
-router.get('/', async function (req, res) {
-  const id = req.query.id;
-  const uid = req.query.uid;
+router.get('/', async (req, res) => {
+  const { id } = req.query;
+  const { uid } = req.query;
 
   if (id) {
     const user = await User.findByPk(id);
     return res.status(200).json(user);
-  } else if (uid) {
+  } if (uid) {
     const user = await User.findOne({
       include: Service,
       where: {
@@ -35,15 +31,13 @@ router.get('/', async function (req, res) {
       },
     });
     return res.status(200).json(user);
-  } else {
-    const allUsers = await User.findAndCountAll();
-    return res.status(200).json(allUsers);
   }
+  const allUsers = await User.findAndCountAll();
+  return res.status(200).json(allUsers);
 });
 
-
-router.get('/friends', async function (req, res) {
-  const uid = req.query.uid;
+router.get('/friends', async (req, res) => {
+  const { uid } = req.query;
   const user = await User.findOne({
     include: Service,
     where: {
@@ -51,9 +45,9 @@ router.get('/friends', async function (req, res) {
     },
   });
 
-  const [notfriends, metadata] = await sequelize.query(
-    'SELECT * FROM USERS WHERE USERS.ID NOT IN (SELECT FRIENDSHIPS.USERID FROM FRIENDSHIPS WHERE FRIENDSHIPS.FRIENDID='+ user.Id +') AND USERS.ID<>'+user.Id
-);
+  const [notfriends] = await sequelize.query(
+    `SELECT * FROM USERS WHERE USERS.ID NOT IN (SELECT FRIENDSHIPS.USERID FROM FRIENDSHIPS WHERE FRIENDSHIPS.FRIENDID=${user.Id}) AND USERS.ID<>${user.Id}`,
+  );
   return res.status(200).json(notfriends);
 });
 

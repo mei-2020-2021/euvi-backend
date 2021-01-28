079ca6b603ae03347f4919af143fa9bb9e899881
Ricardo Faria <ricardofaria@MacBook-Pro-de-Ricardo.local>
2021-01-28 22:01:22 +0000

    cenas

--- routes/genre.handler.js
+++ routes/genre.handler.js
@@ -1,21 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
-const sequelize = require('../sequelize/_index');
 const Genre = require('../sequelize/models/genre.model');
 
-
-router.get('/', async function (req, res){
-  var allGenres = [];
-
-  await Genre.findAll({
-  }).then((all)=>{
-    for(var i=0; i<all.length;i++){
-      allGenres.push({Id: all[i].Id,Name: all[i].Value})
-    }
-  })
-  return res.status(200).json(allGenres)
-})
-
-
+router.get('/', async (req, res) => {
+  const genres = await Genre.findAll();
+  return res.status(200).json(genres.map((genre) => ({ id: genre.Id, name: genre.Value })));
+});
 
 module.exports = router;
